560a4b45df91db0df8e9ec31de448047bead0153
Iulia <ionescu.iuliamaria@gmail.com>
2018-07-11 16:17:47 +0100

    Highlight column and row on hover

--- src/Our.Umbraco.CheckboxTable/Resources/CheckboxTable.js
+++ src/Our.Umbraco.CheckboxTable/Resources/CheckboxTable.js
@@ -72,7 +72,18 @@
             }
 
             $scope.model.value.cells = cells;
-        }
+		}
+
+	    $("table").delegate('td', 'mouseover mouseleave', function (e) {
+		    if (e.type == 'mouseover') {
+			    $(this).parent().addClass("hover");
+			    $("colgroup").eq($(this).index()).addClass("hover");
+		    }
+		    else {
+			    $(this).parent().removeClass("hover");
+			    $("colgroup").eq($(this).index()).removeClass("hover");
+		    }
+	    });
     }
 
 })();
